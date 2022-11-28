10ad84a980c93c525bf072843acb524f6f538075
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2022-11-27 21:58:55 -0300

    Side bar and main bar implementation

---
+++ src/components/MainBar.js
@@ -0,0 +1,11 @@
+import './css/MainBar.css'
+
+function MainBar(){
+    return(
+        <div className="mainbar">
+            <h2>Clinical Name</h2>
+        </div>
+    )
+}
+
+export default MainBar
