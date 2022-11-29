5d58104bb205547bb975749e2e86eb20b3dc9397
Giulianobrjf <giuliano.leite2002@gmail.com>
2022-11-29 00:41:47 -0300

    Deleted Navbar and SideBar

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import "./css/Button.css";
+
+const Button = ({children, onClick}) => {
+    return (
+        
+        <button className='button' onClick={onClick}>
+            {children}
+        </button>
+
+
+
+      );
+}
+export default Button;
